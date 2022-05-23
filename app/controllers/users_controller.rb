4cbef05de1ee42fc820414be23b88b295aa4f47e
Mono <snayper00001@gmail.com>
2022-05-23 14:00:55 +0300

    Complete alpha-blog, ready to use

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, only: [:edit, :update]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
 def show
   @articles = @user.articles
@@ -24,6 +26,13 @@ def create
   end
 end
 
+def destroy
+  @user.destroy
+  session[:user_id] = nil if @user == current_user
+  flash[:notice] = "Account and all associated articles successfully deleted"
+  redirect_to articles_path
+end
+
 def edit
 
 end
@@ -46,6 +55,13 @@ end
 
 def user_params
   params.require(:user).permit(:username, :email, :password)
+end
+
+def require_same_user
+  if current_user != @user && !current_user.admin?
+      flash[:alert] = "You can only edit or delete your own profile"
+      redirect_to @user
+  end
 end
 
 end
